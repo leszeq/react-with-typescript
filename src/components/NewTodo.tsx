f39e0555246ac48972b886e7df22d77c9b7ac9a1
leszeq <37637730+leszeq@users.noreply.github.com>
2022-09-30 17:04:45 +0200

    adding css files

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,20 +1,22 @@
 import React, { useRef } from 'react';
 
-type NewTodoProps = {
-    onAddTodo: (todoText: string) => void;
-}
+import './NewTodo.css';
 
-const NewTodo: React.FC <NewTodoProps> = props => {
-    const textInputRef = useRef<HTMLInputElement>(null);
+type NewTodoProps = {
+	onAddTodo: (todoText: string) => void;
+};
+
+const NewTodo: React.FC<NewTodoProps> = (props) => {
+	const textInputRef = useRef<HTMLInputElement>(null);
 	const todoSubmitHandler = (event: React.FormEvent) => {
 		event.preventDefault();
-        const enteredText = textInputRef.current!.value;
-        props.onAddTodo(enteredText);
+		const enteredText = textInputRef.current!.value;
+		props.onAddTodo(enteredText);
 	};
 
 	return (
 		<form onSubmit={todoSubmitHandler}>
-			<div>
+			<div className='form-control'>
 				<label htmlFor='todo-text'>Todo Text</label>
 				<input type='text' id='todo-text' ref={textInputRef} />
 			</div>
